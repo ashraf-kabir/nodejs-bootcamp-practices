caf3b5f4c62c293931df899b0471914c6670a501
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-02-13 21:09:45 +0600

    Create index.js

---
+++ 02-intro-to-nodejs/08/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    //console.log(req);
+    res.end('Hello from the server!');
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('listening to request on port 8000')
+});
