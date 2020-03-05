b664055562fb2b43e265a0c9721979b82b462837
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-03-05 17:20:44 +0600

    promise

--- 05/03/index.js
+++ 05/03/index.js
@@ -4,12 +4,16 @@ const superagent = require('superagent');
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
     console.log(`Breed: ${data}`);
 
-    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err, res) => {
-        if (err) return console.log(err.message);
+    superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .then(res => {
         console.log(res.body.message);
 
         fs.writeFile('dog-img.txt', res.body.message, err => {
+            if (err) return console.log(err.message);
             console.log('Random dog image saved to file')
         });
+    }).catch(err => {
+        console.log(err.message);
     });
 });
