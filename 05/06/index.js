709cc882ce43d31385dd184a5d201b27cd4e81de
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-03-06 10:16:34 +0600

    updated

---
+++ 05/06/index.js
@@ -0,0 +1,76 @@
+const fs = require('fs');
+const superagent = require('superagent');
+
+const readFilePro = file => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, (err, data) => {
+            if (err) reject('could not find that file');
+            resolve(data);
+        });
+    });
+};
+
+const writeFilePro = (file, data) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(file, data, err => {
+            if (err) return('Could not write the file');
+            resolve('success');
+        });
+    });
+};
+
+const getDogPic = async () => {
+    try {
+        const data = await readFilePro(`${__dirname}/dog.txt`);
+        console.log(`Breed: ${data}`);
+
+        const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+        console.log(res.body.message);
+
+        await writeFilePro('dog-img.txt', res.body.message);
+        console.log('Random dog image saved to file');
+    } catch (err) {
+        console.log(err);
+        throw(err);
+    }
+    return '2: Ready';
+};
+
+(async() => {
+    try {
+        console.log('1: will get dog pics');
+        const x = await getDogPic();
+        console.log(x);
+        console.log('3: done with getting dog pics');
+    } catch(err) {
+        console.log('Error');
+    }
+})();
+
+/*
+console.log('1: will get dog pics');
+getDogPic().then(x => {
+    console.log(x);
+    console.log('3: done with getting dog pics');
+}).catch(err => {
+    console.log('Error');
+});
+*/
+
+/*
+readFilePro(`${__dirname}/dog.txt`)
+.then(data => {
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+})
+.then(res => {
+    console.log(res.body.message);
+    return writeFilePro('dog-img.txt', res.body.message);
+})
+.then(() => {
+    console.log('Random dog image saved to file');
+})
+.catch(err => {
+    console.log(err);
+});
+*/
