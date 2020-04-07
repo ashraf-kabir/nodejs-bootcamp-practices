287d8b6d586698ac6d3e9aec1443b4df6737a8a8
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-04-08 00:08:04 +0600

    API handling using GET

--- 06/04/app.js
+++ 06/04/app.js
@@ -1,13 +1,28 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'Hello from the server side!', app: 'natours'});
-});
+// app.get('/', (req, res) => {
+//     res.status(200).json({message: 'Hello from the server side!', app: 'natours'});
+// });
 
-app.post('/', (req, res) => {
-    res.send('You can post to this endpoint...');
+// app.post('/', (req, res) => {
+//     res.send('You can post to this endpoint...');
+// })
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours: tours
+        }
+    })
 })
 
 const port = 3000;
